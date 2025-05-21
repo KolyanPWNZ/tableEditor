6696ffcdea49d773d1c763ba409a052b88cf198c
Nikolay Nikitin <krukolnik@gmail.com>
2025-05-21 20:59:04 +0300

    impl ui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,174 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include "delegates/numberdelegate.h"
+#include "delegates/datedelegate.h"
+#include "delegates/booldelegate.h"
+#include "delegates/stringdelegate.h"
+#include "serializer/xmlserializer.h"
+#include "parser/xmlparser.h"
+#include <QFileDialog>
+#include <QSharedPointer>
+#include <QInputDialog>
+#include <QDate>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    setModel(QSharedPointer<TableModel>::create(this));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::setModel(QSharedPointer<TableModel> model)
+{
+    m_model = model;
+    m_sortModel = QSharedPointer<NaturalSort>::create(this);
+    m_sortModel->setSourceModel(m_model.data());
+    ui->tableView->setModel(m_sortModel.data());
+    ui->tableView->setSortingEnabled(true);
+
+    updateDelegates();
+}
+
+void MainWindow::on_pbLoadFile_clicked()
+{
+    QString pathFile = QFileDialog::getOpenFileName(this, "Open XML file", "", "XML files (*.xml *.XML *.xmlss *.XMLSS)");
+    if (pathFile.isEmpty()) return;
+
+    auto modelNew =  QSharedPointer<TableModel>::create(this);
+    if (!XmlParser::loadFromFile(pathFile, modelNew.data()))
+        return;
+    setModel(modelNew);
+}
+
+void MainWindow::on_pbSaveFile_clicked()
+{
+    QString pathFile = QFileDialog::getSaveFileName(this, "Save as XML", "", "XML files (*.xml *.xmlss)");
+    if (!pathFile.isEmpty())
+        XmlSerializer::saveToFile(pathFile, m_model.data());
+}
+
+void MainWindow::updateDelegates()
+{
+    for (int col = 0; col < m_model->columnCount(); ++col)
+        ui->tableView->setItemDelegateForColumn(col, nullptr);
+
+    if (m_model) {
+        for (int col = 0; col < m_model->columnCount(); ++col) {
+            switch (m_model->columnType(col)) {
+            case ColumnTypeUtils::BooleanType:
+                ui->tableView->setItemDelegateForColumn(col, new BoolDelegate(this));
+                break;
+            case ColumnTypeUtils::DateType:
+                ui->tableView->setItemDelegateForColumn(col, new DateDelegate(this));
+                break;
+            case ColumnTypeUtils::NumberType:
+                ui->tableView->setItemDelegateForColumn(col, new NumberDelegate(this));
+                break;
+            case ColumnTypeUtils::StringType:
+                ui->tableView->setItemDelegateForColumn(col, new StringDelegate(this));
+                break;
+            default:
+                break;
+            }
+        }
+    }
+}
+
+void MainWindow::on_pbAddColumn_clicked()
+{
+    bool ok = false;
+    QString columnName = QInputDialog::getText(this, "Column Name", "Write column name:", QLineEdit::Normal, "", &ok);
+    if (!ok || columnName.isEmpty())
+        return;
+
+    ColumnTypeUtils::ColumnType type = askColumnType();
+    if (type == ColumnTypeUtils::InvalidType)
+        return;
+
+    int col = m_model->columnCount();
+    m_model->insertColumn(col);
+    m_model->setHeaderData(col, Qt::Horizontal, columnName, Qt::DisplayRole);
+    m_model->setColumnType(col, type);
+    updateDelegates();
+}
+
+ColumnTypeUtils::ColumnType MainWindow::askColumnType()
+{
+    QStringList items = {"String", "Number", "DateTime", "Boolean"};
+    bool ok = false;
+    QString item = QInputDialog::getItem(this, "Тип столбца", "Выберите тип:", items, 0, false, &ok);
+    if (!ok) return ColumnTypeUtils::InvalidType;
+
+    return ColumnTypeUtils::fromString(item);
+}
+
+void MainWindow::on_pbAddRow_clicked()
+{
+    if (!m_model) return;
+    int row = m_model->rowCount();
+    m_model->insertRow(row);
+
+    for (int col = 0; col < m_model->columnCount(); ++col) {
+        QVariant defaultValue;
+
+        switch (m_model->columnType(col)) {
+        case ColumnTypeUtils::StringType:
+            defaultValue = "";
+            break;
+        case ColumnTypeUtils::NumberType:
+            defaultValue = 0.0;
+            break;
+        case ColumnTypeUtils::BooleanType:
+            defaultValue = false;
+            break;
+        case ColumnTypeUtils::DateType:
+            defaultValue = QDate::currentDate();
+            break;
+        }
+        m_model->setData(m_model->index(row, col), defaultValue);
+    }
+}
+
+void MainWindow::on_pbDeleteRow_clicked()
+{
+    if (!m_model) return;
+
+    QModelIndexList selected = ui->tableView->selectionModel()->selectedRows();
+    if (selected.isEmpty()) return;
+
+    std::sort(selected.begin(), selected.end(), [](const QModelIndex &a, const QModelIndex &b) {
+        return a.row() > b.row();
+    });
+
+    for (const QModelIndex &index : selected) {
+        int sourceRow = m_sortModel->mapToSource(index).row();
+        m_model->removeRow(sourceRow);
+    }
+}
+
+void MainWindow::on_pbDeleteColumn_clicked()
+{
+    if (!m_model) return;
+
+    QModelIndexList selected = ui->tableView->selectionModel()->selectedColumns();
+    if (selected.isEmpty()) return;
+
+    std::sort(selected.begin(), selected.end(), [](const QModelIndex &a, const QModelIndex &b) {
+        return a.column() > b.column();
+    });
+
+    for (const QModelIndex &index : selected) {
+        int sourceRow = m_sortModel->mapToSource(index).column();
+        m_model->removeColumn(sourceRow);
+    }
+}
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,12 +2,17 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "tablemodel.h"
+#include "NaturelSort.h"
+#include <QAbstractItemDelegate>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
 }
 QT_END_NAMESPACE
+
 
 class MainWindow : public QMainWindow
 {
@@ -17,7 +22,25 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pbLoadFile_clicked();
+    void on_pbSaveFile_clicked();
+
+    void on_pbAddRow_clicked();
+    void on_pbAddColumn_clicked();
+    void on_pbDeleteRow_clicked();
+    void on_pbDeleteColumn_clicked();
+
+private:
+    void updateDelegates();
+    void setModel(QSharedPointer<TableModel> model);
+    ColumnTypeUtils::ColumnType askColumnType();
+
 private:
     Ui::MainWindow *ui;
+    QSharedPointer<TableModel> m_model;
+    QSharedPointer<NaturalSort> m_sortModel;
+    QMap<int, QAbstractItemDelegate*> m_delegates;
 };
+
 #endif // MAINWINDOW_H
